0d39455c15f7a1f9e59aed8a2290075c59d34157
albertusYoga30 <albertus.yoga3003@gmail.com>
2022-11-24 15:11:54 +0700

    add place holder in recipe fragment

--- app/src/main/java/com/example/modernrecipes/RecipesFragment.kt
+++ app/src/main/java/com/example/modernrecipes/RecipesFragment.kt
@@ -5,16 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.modernrecipes.databinding.FragmentRecipesBinding
+
 
 class RecipesFragment : Fragment() {
+
+    private var _binding : FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        _binding = FragmentRecipesBinding.inflate(layoutInflater, container, false)
+
+
+        return binding.root
     }
 
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
