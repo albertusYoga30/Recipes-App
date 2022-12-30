cd35e37059ed59b10367a3a7c6403a7e9969d429
albertusYoga30 <albertus.yoga3003@gmail.com>
2022-12-30 15:59:23 +0700

    favorite recipes fragments

--- app/src/main/java/com/example/modernrecipes/ui/fragments/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/example/modernrecipes/ui/fragments/favorites/FavoriteRecipesFragment.kt
@@ -5,18 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.modernrecipes.R
+import com.example.modernrecipes.adapters.FavoriteRecipesAdapter
+import com.example.modernrecipes.databinding.FragmentFavoriteRecipesBinding
+import com.example.modernrecipes.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class FavoriteRecipesFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteRecipesBinding? = null
+    private val binding get() = _binding!!
+
+    private val mAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter()}
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        _binding = FragmentFavoriteRecipesBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
+        binding.mAdapter = mAdapter
+
+        setupRecyclerView(binding.favoritesRecipesRecyclerView)
+
+        return binding.root
     }
 
+    private fun setupRecyclerView(recyclerView: RecyclerView){
+        recyclerView.adapter = mAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
